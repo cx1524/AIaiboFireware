4d6d9437e4956ad825517a267c7372facbdd0bc0
cx1524 <69176663+cx1524@users.noreply.github.com>
2026-04-10 09:58:46 +0800

    update(main): update base frame

--- src/main.c
+++ src/main.c
@@ -2,10 +2,19 @@
 #include "usbd_cdc_acm.h"
 
 #include <stdio.h>
+#include "device_cfg.h"
 
 int main(void) {
-    printf("Hello, world!\n");
-    return 0;
+    //printf("Hello, world!\n");
+    device_config();
+
+    while (1)
+    {
+        delay_ms(500);
+        GPIO_SetBit(GPIOB, GPIO_PIN_6 | GPIO_PIN_7);
+        delay_ms(500);
+        GPIO_ResetBit(GPIOB, GPIO_PIN_6 | GPIO_PIN_7);
+    }
 }
 
 
